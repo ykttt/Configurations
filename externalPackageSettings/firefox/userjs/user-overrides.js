c47708a1cb280b24c139138b1c12cd18b7ba90c3
J. Peng <ykttt@tuta.io>
2024-11-01 22:40:09 +0900

    slightly change

--- externalPackageSettings/firefox/userjs/user-overrides.js
+++ externalPackageSettings/firefox/userjs/user-overrides.js
@@ -2,7 +2,7 @@
 /* use arkenfox user.js */
 
 user_pref("browser.startup.page", 1);
-user_pref("browser.startup.homepage", "moz-extension://9b379887-f257-4d7e-b7b7-e9580ce974f5/index.html");
+user_pref("browser.startup.homepage", "moz-extension://10a994bb-ecfa-4e14-9825-56ce92fa50f6/index.html");
 user_pref("browser.compactmode.show", true);
 user_pref("privacy.clearOnShutdown.history", false);
 user_pref("privacy.resistFingerprinting.letterboxing", false);

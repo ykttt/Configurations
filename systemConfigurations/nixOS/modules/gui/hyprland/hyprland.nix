58ecdd4ecad873d314bb1aafe368ccde166ff752
J. Peng <ykttt@tuta.io>
2024-11-08 02:29:10 +0900

    slightly change

--- systemConfigurations/nixOS/modules/gui/hyprland/hyprland.nix
+++ systemConfigurations/nixOS/modules/gui/hyprland/hyprland.nix
@@ -22,16 +22,7 @@
     pkgs,
     ...
   }: {
-    xdg.desktopEntries."org.gnome.Settings" = {
-      name = "Settings";
-      comment = "Gnome Control Center";
-      icon = "org.gnome.Settings";
-      exec = "env XDG_CURRENT_DESKTOP=gnome ${pkgs.gnome-control-center}/bin/gnome-control-center";
-      categories = ["X-Preferences"];
-      terminal = false;
-    };
     home.packages = with pkgs; [
-      gnome-control-center
       wlsunset
       hyprlock
     ];
--- systemConfigurations/nixOS/modules/hosts/x86Laptop/nixConfig.nix
+++ systemConfigurations/nixOS/modules/hosts/x86Laptop/nixConfig.nix
@@ -21,4 +21,5 @@
       ];
     };
   };
+  documentation.nixos.enable = false;
 }

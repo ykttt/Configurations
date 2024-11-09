bbbed57b9a6c9abf60fbf446ac9bf4d09be3edd1
J. Peng <ykttt@tuta.io>
2024-11-09 16:26:58 +0900

    slightly change

--- systemConfigurations/nix/modules/applications/gaming/aagl/default.nix
+++ systemConfigurations/nix/modules/applications/gaming/aagl/default.nix
@@ -3,14 +3,10 @@
 # aagl/default.nix
 
 {
-  home-manager.users.km = {
-    inputs,
-    pkgs,
-    ...
-  }: {
-    imports = [ inputs.aagl.nixosModules.default ];
-    nix.settings = inputs.aagl.nixConfig; # Set up Cachix
-    programs.anime-games-launcher.enable = true;
-    home.stateVersion = "24.05";
-  };
+  inputs,
+  ...
+}: {
+  imports = [ inputs.aagl.nixosModules.default ];
+  nix.settings = inputs.aagl.nixConfig; # Set up Cachix
+  programs.anime-games-launcher.enable = true;
 }

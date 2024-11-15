bbd24621c22cbe51560ecfb0a7fc08f2489eb8d9
J. Peng <ykttt@tuta.io>
2024-11-15 22:11:29 +0900

    change sddm settings

--- systemConfigurations/nix/modules/gui/general/sddm.nix
+++ systemConfigurations/nix/modules/gui/general/sddm.nix
@@ -16,12 +16,16 @@
   environment.systemPackages = [(
     pkgs.where-is-my-sddm-theme.override {
       themeConfig.General = {
-        background = "/home/km/.local/share/backgrounds/bg.png";
-        backgroundMode = "none";
+        # background = "/home/km/.local/share/backgrounds/bg.png";
+        # blurRadius = 10;
+        backgroundFill = "#24273a";
+        backgroundFillMode= "aspect";
         passwordInputCursorVisible = true;
-        showSessionsByDefault = true;
-        blurRadius = 10;
+        showSessionsByDefault = false;
+        # passwordCursorColor = "random";
+        passwordCursorColor = "#f2d5cf";
         basicTextColor = "#babbf1";
+        passwordTextColor = "#eebebe";
         hideCursor = true;
       };
     }

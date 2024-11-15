9ee869004d262270f386a2415ff845501431a181
J. Peng <ykttt@tuta.io>
2024-11-15 20:09:27 +0900

    change sddm theme overrides

--- systemConfigurations/nix/modules/gui/general/sddm.nix
+++ systemConfigurations/nix/modules/gui/general/sddm.nix
@@ -13,5 +13,17 @@
     theme = "where_is_my_sddm_theme";
     extraPackages = [ pkgs.kdePackages.qt5compat ];
   };
-  environment.systemPackages = [ pkgs.where-is-my-sddm-theme ];
+  environment.systemPackages = [(
+    pkgs.where-is-my-sddm-theme.override {
+      themeConfig.General = {
+        background = "/home/km/.local/share/backgrounds/bg.png";
+        backgroundMode = "none";
+        passwordInputCursorVisible = true;
+        showSessionsByDefault = true;
+        blurRadius = 10;
+        basicTextColor = "#babbf1";
+        hideCursor = true;
+      };
+    }
+  )];
 }

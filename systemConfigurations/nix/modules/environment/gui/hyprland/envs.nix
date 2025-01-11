215166e38f7c70e1eca242a8d0686cd33e93b705
J. Peng <ykttt@tuta.io>
2025-01-12 05:35:48 +0900

    change XCURSOR_SIZE in hyprland

--- systemConfigurations/nix/modules/environment/gui/hyprland/envs.nix
+++ systemConfigurations/nix/modules/environment/gui/hyprland/envs.nix
@@ -4,7 +4,7 @@
   home-manager.users.km = {pkgs, ...}: {
     wayland.windowManager.hyprland.settings.env = [
       "GDK_SCALE,2"
-      "XCURSOR_SIZE,20"
+      "XCURSOR_SIZE,24"
       "HYPRCURSOR_SIZE,24"
     ];
     home.stateVersion = "24.05";

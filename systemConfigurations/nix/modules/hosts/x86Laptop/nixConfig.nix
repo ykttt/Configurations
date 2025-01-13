161d3d0a385e05d305c2d6a282272921f663bfd9
J. Peng <ykttt@tuta.io>
2025-01-13 17:47:13 +0900

    delete wezterm cache source

--- systemConfigurations/nix/modules/hosts/x86Laptop/nixConfig.nix
+++ systemConfigurations/nix/modules/hosts/x86Laptop/nixConfig.nix
@@ -13,11 +13,9 @@
       experimental-features = ["nix-command" "flakes"];
       substituters = [
         "https://cache.nixos.org"
-        "https://wezterm.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
       ];
     };
   };
